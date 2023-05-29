5b7d1b59d10f53138a056ddcf6815d1d80fa5628
Lukasz Gumienniczuk <01178841@pw.edu.pl>
2023-05-29 12:57:27 +0200

    decompressor is working on 12 and 16 compressionRatio. Also i added some test files for 12 and 16 compression ratio

--- src/main/java/decompressor/Decompressor.java
+++ src/main/java/decompressor/Decompressor.java
@@ -8,30 +8,74 @@ public class Decompressor {
 
     private HashMap<String, Short> huffmanMap = new HashMap<>();
     private BufferedOutputStream writer;
-
+    private byte buffor = 0;
+    private int bufforIndex = 0;
     Decompressor(Dictionary dictionary) throws IOException {
         // Create a map for quick lookup of the Huffman code to its bit representation
         for (Word word : dictionary.dictionary) {
             huffmanMap.put(word.code, word.bitRepresentation);
         }
-        writer = new BufferedOutputStream(new FileOutputStream("wyniki.txt"));
+        writer = new BufferedOutputStream(new FileOutputStream("wyniki.jpeg"));
     }
-
-    public void decompress(char[] dataToDecompress) throws IOException {
+    private void write8Bits(short toWrite) throws IOException {
+        writer.write((byte) toWrite);
+    }
+    private void write12Bits(short bits) throws IOException {
+        if(this.bufforIndex == 0){
+            short toWrite = (short) ((bits >>4) & 0x00FF);
+            this.buffor = (byte) (bits << 4);
+            write8Bits(toWrite);
+            this.bufforIndex = 4;
+        }else{
+            short toWrite = (short) (this.buffor | ((bits >> 8) & 0x00FF));
+            write8Bits(toWrite);
+            write8Bits(bits);
+            bufforIndex = 0;
+        }
+    }
+    private void write16Bits(short bits) throws IOException {
+        write8Bits((short) (bits >> 8));
+        write8Bits((short) (bits));
+    }
+    private void writeToFile(short toWrite, int compressionRatio) throws IOException {
+        switch(compressionRatio){
+            case 8:
+                write8Bits(toWrite);
+                break;
+            case 12:
+                write12Bits(toWrite);
+                break;
+            case 16:
+                write16Bits(toWrite);
+                break;
+        }
+    }
+    public void decompress(char[] dataToDecompress, int compressionRatio, int restControl, byte restValue) throws IOException {
         // Decompress the data
         ArrayList<Byte> toWrite = new ArrayList<>();
         StringBuilder temp = new StringBuilder();
+
         for (char c : dataToDecompress) {
             temp.append(c);
             Short bitRepresentation = huffmanMap.get(temp.toString());
             if (bitRepresentation != null) {
-
-                toWrite.add ((byte) bitRepresentation.shortValue());
+//                toWrite.add ((byte) bitRepresentation.shortValue());
+                writeToFile(bitRepresentation.shortValue(), compressionRatio);
                 temp.setLength(0);  // Reset the temp string builder
             }
         }
-        for(int i=0;i<toWrite.size();i++){
-            writer.write(toWrite.get(i));
+//        for(int i=0;i<toWrite.size();i++){
+//            writer.write(toWrite.get(i));
+//            writeToFile(toWrite.get(i), compressionRatio, buffor, bufforIndex);
+//        }
+        switch(restControl){
+            case 4:
+                buffor |= ((restValue>>4) & 0x000F);
+                write8Bits(buffor);
+                break;
+            case 8:
+                write8Bits(restValue);
+                break;
         }
     }
 
--- src/main/java/decompressor/Main.java
+++ src/main/java/decompressor/Main.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class Main{
     public static void main(String[] args){
         DataReader data = new DataReader();
-        if(!data.readData("testFiles/pan_tadeusz_compressed.txt")){
+        if(!data.readData("testFiles/test2_compressed_16.jpeg")){
             System.exit(1);
         }
         SumController sumController = new SumController(data);
@@ -16,6 +16,7 @@ public class Main{
             System.exit(1);
         }
         sumController.getValuesFromFile(data);
+        sumController.printSums();
         Dictionary dictionary = new Dictionary();
         dictionary.getDictionary(data, sumController.compressionRatio);
         if(!FileValidator.checkCorrectnessOfDictionary()){
@@ -30,7 +31,7 @@ public class Main{
         char[] dataToDecompress = compressedData.getCompressedData(data.getData(), sumController.rest1, sumController.rest2);
         try {
             Decompressor decompressor = new Decompressor(dictionary);
-            decompressor.decompress(dataToDecompress);
+            decompressor.decompress(dataToDecompress, sumController.compressionRatio, sumController.rest2, compressedData.rest2Value);
             decompressor.close();
         } catch (IOException e) {
             e.printStackTrace();
